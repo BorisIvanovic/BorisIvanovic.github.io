81e9654875d15644473a37e9cadf921f62c49a2e
BorisIvanovic <borisi@cs.stanford.edu>
2018-11-19 03:11:44 -0800

    No more one-pager!

--- js/main.js
+++ js/main.js
@@ -1,24 +1,8 @@
-(function($) {
-
-	"use strict";	
-
-  
-    $('.navigation').singlePageNav({
-        currentClass : 'active'
-    });
-
-
-    $('.toggle-menu').click(function(){
-        $('.responsive-menu').stop(true,true).slideToggle();
-        return false;
-    });
-
-
-
-})(jQuery);
-
-
-
-
-
-
+$(document).ready(function() {
+    var e = location.href.toLowerCase();
+    $(".navigation li a").each(function() {
+        e.indexOf(this.href.toLowerCase()) > -1 && ($("li.highlight").removeClass("highlight"), $(this).parent().addClass("highlight"))
+    })
+}), $(".toggle-menu").click(function() {
+    return $(".responsive-menu").stop(!0, !0).slideToggle(), !1
+});
